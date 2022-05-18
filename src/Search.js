bb3972bc03f3beac0f739465df43b6995f142e59
Anthony DeVarti <anthony.devarti@student.awesomeinc.org>
2022-05-18 04:10:22 +0000

    condition changes price after searching

--- src/Search.js
+++ src/Search.js
@@ -1,53 +1,89 @@
 import { useEffect, useState } from "react";
-import { Form, FormControl, Button } from "react-bootstrap"
+import { Form, FormControl, Button, Row } from "react-bootstrap"
 import Visualizer from "./Visualizer";
 
 export default function Search() {
 
     var scryfall = require("scryfall-client");
 
-    const [currentCard, setCurrentCard] = useState(
-        {
-            // name: "",
-            // img: "",
-            // price: "",
-            // foilPrice: "",
-            // text: "",
-            // printings: "",
-            // tcgId: "",
-            // set: "",
-        }
-    )
+    const [currentCard, setCurrentCard] = useState({})
 
+    //foil state
+    const options = [
+        { label: 'Non-Foil', value: 'non' },
+        { label: 'Foil', value: 'foil' },
+    ];
+
+    const [foil, setFoil] = useState("non")
+
+    const handleFoilChange = (event) => {
+        setFoil(event.target.value);
+    };
+
+    //condition state
+    const conditions = [
+        { label: "Near-Mint", value: 1 },
+        { label: "Lightly Played", value: .8 },
+        { label: "Moderately Played", value: .7 },
+        { label: "Heavily Played", value: .5 },
+        { label: "Damaged", value: .25 },
+    ]
+
+    const [condition, setCondition] = useState(1)
+
+    //changes the condition state object based on the dropdown
+    const handleConditionChange = (event) => {
+        setCondition(event.target.value);
+    };
+
+    
     function handleSearch(e) {
         e.preventDefault()
         scryfall
-            .get("cards/named", {
-                fuzzy: "animar soul",
-            })
-            .then(function (card) {
+        .get("cards/named", {
+            fuzzy: "animar soul",
+        })
+        .then(function (card) {
                 card.getPrice(); // '11.25'
                 card.getPrice("usd"); // '11.25'
                 card.getPrice("usd_foil"); // '52.51'
-                // console.log(card.name)
+                console.log(condition)
                 let result = {
                     name: card.name,
                     img: card.image_uris.normal,
-                    price: card.prices.usd,
-                    foilPrice: card.prices.usd_foil,
+                    price: Math.floor((card.prices.usd * condition)*100)/100,
+                    foilPrice: card.prices.usd_foil * condition,
                     text: card.oracle_text,
                     tcgId: card.prints_search_uri,
                     set: card.set_name,
                 }
-                console.group(card)
+                // console.group(card)
                 localStorage.setItem("card", JSON.stringify(result))
                 setCurrentCard(result)
+                // console.log(card)
             });
-    }
-
-    // localStorage.setItem()
-    return (
-        <>
+        }
+        
+        return (
+            <>
+            <div className="narrow-row">
+                <label>
+                    Foiling
+                    <select foil={foil} onChange={handleFoilChange}>
+                        {options.map((option) => (
+                            <option value={option.value}>{option.label}</option>
+                        ))}
+                    </select>
+                </label>
+                <label>
+                    Condition
+                    <select condition={condition} onChange={handleConditionChange}>
+                        {conditions.map((condition) => (
+                            <option key={condition.value} value={condition.value}>{condition.label}</option>
+                        ))}
+                    </select>
+                </label>
+            </div>
             <div className="search-bar">
                 <Form className="d-flex" onSubmit={(e) => handleSearch(e)}>
                     <FormControl
@@ -59,7 +95,7 @@ export default function Search() {
                     <Button variant="outline-success" onClick={(e) => handleSearch(e)}>Search</Button>
                 </Form>
             </div>
-            <Visualizer currentCard={currentCard}/>
+            <Visualizer currentCard={currentCard} />
         </>
     )
 }
--- src/Visualizer.js
+++ src/Visualizer.js
@@ -2,62 +2,11 @@ import { Col, Row, Button, Form } from "react-bootstrap"
 import { useState } from "react"
 
 export default function Visualizer(currentCard) {
-    console.log(currentCard)
+    console.log(currentCard.currentCard)
 
-    let price = currentCard.currentCard.price
-    let foilPrice = currentCard.currentCard.foilPrice
+    //handles the current offer
+    const [offer, setOffer] = useState(0)
 
-    //foil state
-    const options = [
-        { label: 'Non-Foil', value: 'non' },
-        { label: 'Foil', value: 'foil' },
-    ];
-
-    const [foil, setFoil] = useState("non")
-
-    const handleChange = (event) => {
-        setFoil(event.target.value);
-      };
-
-    //condition state
-    const conditions = [
-        { label: "Near-Mint", value: "nm" },
-        { label: "Lightly Played", value: "lp" },
-        { label: "Moderately Played", value: "mp" },
-        { label: "Heavily Played", value: "hp" },
-        { label: "Damaged", value: "dmg" },
-    ]
-
-    const [condition, setCondition] = useState("nm")
-
-    const handleConditionChange = (event) => {
-        setCondition(event.target.value);
-      };
-
-
-    //determine estimated value
-    //these returns will be determined by settings later, but are hc for now
-    function condMod(condition){
-        if (condition == "nm"){
-            return 1
-        } else if (condition == "lp"){
-            return .9
-        } else if (condition == "mp"){
-            return .75
-        } else if (condition == "hp"){
-            return .6
-        } else if (condition == "dmg"){
-            return .25
-        } else return 1
-    }
-    
-    
-    function getPrice(foiling){
-        if (foil == "foil") {
-            return foilPrice
-        } else return price
-    }
-    //need to find a way to make the price that is displayed in the dom update on change.
 
     return (
         <div className="visualizer">
@@ -91,27 +40,7 @@ export default function Visualizer(currentCard) {
                     <h2>Price</h2>
                 </Row>
                 <Row>
-                    <label>
-                        Foiling
-                        <select foil={foil} onChange={handleConditionChange}>
-                            {options.map((option) => (
-                                <option value={option.value}>{option.label}</option>
-                            ))}
-                        </select>
-                    </label>
-                </Row>
-                <Row>
-                <label>
-                        Condition
-                        <select condition={condition} onChange={handleChange}>
-                            {conditions.map((condition) => (
-                                <option key={condition.value} value={condition.value}>{condition.label}</option>
-                            ))}
-                        </select>
-                    </label>
-                </Row>
-                <Row>
-                    <p>Est Value: {getPrice(foil, condition)}</p>
+                    Suggested Offer: $ {currentCard.currentCard.price}
                 </Row>
                 <Row>
                     <Form className="buy-form">
